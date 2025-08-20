21d1c565ad3e34c302b414e4e1f6ae0c52ee61eb
robertmcarvalho <128416418+robertmcarvalho@users.noreply.github.com>
2025-08-20 19:12:36 -0300

    Delete coop-langchain-full directory

--- coop-langchain-full/agent/tools/pipefy.py
+++
@@ -1,3 +0,0 @@
-import os
-def get_pipefy_link() -> dict:
-    return {"pipefy_url": os.getenv("PIPEFY_URL", "https://app.pipefy.com")}
--- coop-langchain-full/agent/tools/utils.py
+++
@@ -1,3 +0,0 @@
-import datetime as dt
-def iso_now() -> str:
-    return dt.datetime.utcnow().replace(microsecond=0).isoformat() + "Z"
